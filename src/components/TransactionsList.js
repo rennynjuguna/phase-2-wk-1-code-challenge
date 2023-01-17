62cdf7b7e4b32b2fae6cd8b8712bf6fa32611803
Derek Kambo <Tnguyen410@student.gsu.edu>
2023-01-17 15:41:54 +0300

    Added Transaction List Component

---
+++ src/components/TransactionsList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Transaction from "./Transaction";
+function TransactionsList({transactions}) {
+  const list = transactions.map((item)=>{
+    return <Transaction key={item.id} date={item.date} description={item.description} category={item.category} amount={item.amount} />;
+  })
+  return (
+    <table className="ui celled striped padded table">
+      <tbody>
+        <tr>
+          <th>
+            <h3 className="ui center aligned header">Date</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Description</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Category</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Amount</h3>
+          </th>
+        </tr>
+        {/* render a list of <Transaction> components here */}
+        {list}
+      </tbody>
+    </table>
+  );
+}
+
+export default TransactionsList;
