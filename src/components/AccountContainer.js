d7aacba8cdf1ee5e1ca354570faa338a27fbd903
Derek Kambo <Tnguyen410@student.gsu.edu>
2023-01-18 13:16:44 +0300

    Completed changes to components and README

---
+++ src/components/AccountContainer.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import AddTransactionForm from "./AddTransactionForm";
+
+function AccountContainer() {
+  const [transaction, setTransaction] = useState([])
+  const [query, setQuery] = useState("")
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions?q=" + query)
+      .then((resp) => resp.json())
+      .then(transaction => setTransaction(transaction))
+  }, [query])
+  function handleSearch(e) {
+    setQuery(e.target.value)
+  }
+  return (
+    <div>
+      <Search handleSearch={handleSearch} />
+      <AddTransactionForm />
+      <TransactionsList transactions={transaction} />
+    </div>
+  );
+}
+
+export default AccountContainer;
